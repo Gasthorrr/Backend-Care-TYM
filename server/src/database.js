e3121e181524fe92ae71acfb7f9a92dbfac2f34e
Ignacio Payeras <ignaciopayeras7@gmail.com>
2022-10-01 18:23:14 -0300

    Api cadenas medicads

--- server/src/database.js
+++ server/src/database.js
@@ -72,34 +72,7 @@ const getUseruserType = async (key)=>{
 
 };
 
-const login = async (key,password)=>{ 
-    const PrimaryKeyDicctionary={
-        "administrador":"usuario",
-        "cadena_medica":"nombre",
-        "centro_medico":"nombre",
-        "coordinador" : "rut",
-        "medico" : "rut", 
-        "paciente" : "rut" 
-    };
-    const userType=await getUseruserType(key);
-    if(userType=="Not Found"){
-        return "Not Found";
-    }
-    else{
-        const primaryKey = PrimaryKeyDicctionary [userType];
-        queryValidarContraseña = await client.query(`select * from ${userType} where (${primaryKey}='${key}' and contraseña='${password}')`);
-        if(queryValidarContraseña.rowCount==1){
-            return(userType);
-        }
-        return "Incorrect Password";    
-    }
-
-};
-
 module.exports = {
     /// las funciones que se veran al exportar el modulo
     client,
-    testDBAccess,
-    showMedicCenters,
-    login
 }
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 const centerController = require("./../controllers/medicCenter.controller");
-const adminController = require("../controllers/user.controller");
+const adminController = require("./../controllers/user.controller");
+const chainsController = require("./../controllers/medicChain.controller");
 
 router.get("/center/:id",centerController.getMedicCenter);
 router.get('/center',centerController.getMedicCenters);
@@ -14,5 +15,13 @@ router.get('/admins',adminController.getAdministrators);
 router.post('/admins',adminController.addAdmin);
 router.delete("/admins/:usuario",adminController.deleteAdmin);
 router.put("/admins/:usuario",adminController.updateAdmin);
+
+
+router.get("/chain/:name",chainsController.getMedicChain);
+router.get('/chain',chainsController.getMedicChains);
+router.post('/chain',chainsController.addMedicChain);
+router.delete("/chain/:name",chainsController.deleteMedicChain);
+router.put("/chain/:name",chainsController.updateMedicChain);
+
 
 module.exports = router;
