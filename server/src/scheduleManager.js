e1cae04aa9d46a18d037c3a6015269a25ae01a09
JeAntonioLopez <68481514+JeAntonioLopez@users.noreply.github.com>
2022-10-16 11:49:35 -0400

    Obtener bloques disponibles 15%

--- server/src/scheduleManager.js
+++ server/src/scheduleManager.js
@@ -1,4 +1,5 @@
 const client  = require('./database').client;
+const moment = require('moment');
 
 function getDayOfWeek(date) {
     const dayOfWeek = new Date(date).getDay();    
@@ -7,18 +8,29 @@ function getDayOfWeek(date) {
 
   }
 
-  function dateToString(date) {
+  function dateToStringDate(date) {
     const day=date.getDate();
     const month=date.getMonth()+1;
     const year=date.getFullYear();
     return (year+"-"+month+"-"+day);
 
   }
+
+
   function isInside(hour,intervalStart,intervalEnd) {
     if((intervalStart<hour) & (hour<intervalEnd)){
         return true;
     }
     return false;
+  }
+
+  function addMinutes(time,minutes){
+    const s="2022-01-01"+" "+time;
+    const oldDate = new Date(s);
+    var newDate = moment(oldDate).add(minutes, 'm');
+    result = newDate.format('hh:mm:ss');
+    return result;
+
   }
 
 const getAvailableaBlocks = async (date,rut)=>{
@@ -53,9 +65,10 @@ const getAvailableaBlocks = async (date,rut)=>{
           
     }  
     console.log(attentionList);
-
     ////
-    
+    var available=new Array();
+
+
 
 };
 
@@ -63,12 +76,17 @@ const getAvailableaBlocks = async (date,rut)=>{
 
 //// Test
 (async () => {
+    
 
     const query= await client.query('select * from atencion');
     const testDate= query['rows'][0].fecha;
-    const aDate=new Date('15-10-22');
-    getAvailableaBlocks(dateToString(testDate),'19.456.655-k');
+    const s="2022-01-01"+" "+"09:00:00";
+    var aDate=new Date(s);
     const testTime=query['rows'][0].hora_inicio;
+    ///getAvailableaBlocks(dateToStringDate(testDate),'19.456.655-k');
+    const x=addMinutes("09:00:00",10);
+    console.log(x);
+    
 
 }
 )()
