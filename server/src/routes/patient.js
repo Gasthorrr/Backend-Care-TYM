e0c670d54b339d47ad08c4efd72a54442464648a
JeAntonioLopez <68481514+JeAntonioLopez@users.noreply.github.com>
2022-11-19 21:53:27 -0400

    patient routes

--- server/src/routes/patient.js
+++ server/src/routes/patient.js
@@ -3,15 +3,23 @@ const router = Router();
 
 
 const attentionController = require("../controllers/attention.controller");
+const specialtyController = require("../controllers/specialty.controller");
+const medicController = require("../controllers/medic.controller");
+
+//// see available specialties
+router.get("/available_specialties",specialtyController.getSpecialtiesPatient);
+
+//// see available medics
+router.get("/available_medics",medicController.getMedicsPatient);
 
 //// see available attentions
-router.get("/scheduleAttention",attentionController.getAvailableBlocks);
+router.get("/available_attentions",attentionController.getAvailableBlocks);
 
 //// manage his own attentions
-router.get("/attention/:id",attentionController.getAttention);
-router.get("/attention",attentionController.getAttentions);
-router.post("/attention",attentionController.addAttention);
-router.delete("/attention/:id",attentionController.deleteAttention);
+router.get("/my_attentions/:id",attentionController.getAttention);
+router.get("/my_attentions",attentionController.getAttentions);
+router.post("/my_attentions",attentionController.addAttention);
+router.delete("/my_attentions/:id",attentionController.deleteAttention);
 
 
 
