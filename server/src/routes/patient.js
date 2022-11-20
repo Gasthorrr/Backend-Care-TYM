12f6a39152ed8be5b495318d273f7ff6df3d656f
JeAntonioLopez <68481514+JeAntonioLopez@users.noreply.github.com>
2022-11-19 21:16:03 -0400

    get,add and delete attention + schedule atention route

--- server/src/routes/patient.js
+++ server/src/routes/patient.js
@@ -4,7 +4,15 @@ const router = Router();
 
 const attentionController = require("../controllers/attention.controller");
 
+//// see available attentions
 router.get("/scheduleAttention",attentionController.getAvailableBlocks);
+
+//// manage his own attentions
+router.get("/attention/:id",attentionController.getAttention);
+router.get("/attention",attentionController.getAttentions);
+router.post("/attention",attentionController.addAttention);
+router.delete("/attention/:id",attentionController.deleteAttention);
+
 
 
 
